81e482be10fe2e159ddf4c96f8e3cf912c6aac12
BeybladeBurst <73469697+BeybladeBurst@users.noreply.github.com>
2024-06-05 15:00:20 +0800

    Update bey.js

--- customize/bey.js
+++ customize/bey.js
@@ -186,12 +186,11 @@ class Bey extends HTMLElement {
     :host([collapse]) {
         --headfoot:0;
         outline:.1em solid;
-        
-        i {justify-content:space-between;}
-        .part li:not([style]) {display:none;}
-        h4 {align-self:end;}
-        span::before,span:not([title])::after {content:initial !important;}
     }
+    :host([collapse]) i {justify-content:space-between;}
+    :host([collapse]) .part li:not([style]) {display:none;}
+    :host([collapse]) h4 {align-self:end;}
+    :host([collapse]) span::before,:host([collapse]) span:not([title])::after {content:initial !important;}
 
     :host h4 {
         font-size:.7em;
