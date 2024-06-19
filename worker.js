f12c03f092ed3bb0a02af88263dafe9e26b6f37b
bbx <bbx@bbx.com>
2024-06-19 13:03:47 +0800

    3.0

--- worker.js
+++ worker.js
@@ -4,25 +4,46 @@ self.addEventListener('install', ev => {
     ev.waitUntil(Head.cache());
 });
 self.addEventListener('activate', ev => ev.waitUntil(clients.claim()));
-self.addEventListener('fetch', ev => ev.respondWith(
-    (is.internal(ev.request.url) ? caches.match(ev.request, {ignoreSearch: true}) : Promise.resolve())
-    .then(cached => cached ? fetch.net(ev.request) && Head.add(cached) : fetch.net(ev.request))
-    .catch(console.error)
-));
+self.addEventListener('fetch', ev => {
+    if (/sw\/$/.test(new URL(ev.request.url).pathname)) {
+        let query = Object.fromEntries(new URLSearchParams(new URL(ev.request.url).search));
+        query.delete ? respond(query.delete == 'parts' ? 
+            caches.delete('parts') : 
+            caches.open('V3').then(cache => cache.keys()
+                .then(reqs => reqs.forEach(req => new RegExp(`\\.${query.delete}$`).test(req.url) && cache.delete(req)))
+            )
+        , ev) : respond(Promise.resolve());
+    }
+    ev.respondWith(
+        (is.internal(ev.request.url) ? caches.match(ev.request, {ignoreSearch: true}) : Promise.resolve())
+        .then(cached => {
+            if (cached && is.image(ev.request.url))
+                return cached;
+            let fetching = fetch.net(ev.request);
+            return cached ? is.html(ev.request.url) ? Head.add(cached) : cached : fetching;
+        }).catch(console.error)
+    );
+});
+const respond = (action, ev) => action
+    .then(() => ev.respondWith(new Response('', {status: 200})))
+    .catch(er => console.error(er) ?? ev.respondWith(new Response('', {status: 400})));
 
 const is = {
     internal: url => 'go-shoot.github.io' == new URL(url).host,
-    parts: url => /img\/.+?\/.+?\.png$/.test(url),
     cacheable: url => is.internal(url) && !/\.json$/.test(new URL(url).pathname),
+    volatile: url => /\.(?:css|js|json)$/.test(new URL(url).pathname),
+    image: url => /\.(?:ico|svg|jpeg|jpg|png)$/.test(new URL(url).pathname),
+    part: url => /img\/.+?\/.+?\.png$/.test(new URL(url).pathname),
+    html: url => /(?:\/|\.html)$/.test(new URL(url).pathname)
 }
 fetch.net = req => {
-    /css|js|json$/.test(req.url) && (req.url += `?${Math.random()}`);
+    is.volatile(req.url) && (req.url += `?${Math.random()}`);
     return fetch(req).then(res => 
-        (res.status == 200 && is.cacheable(req.url) ? fetch.cache(res) : Promise.resolve(res))
-        .then(Head.add)
-    ).catch(console.error);
+        (res.status < 400 && is.cacheable(req.url) ? fetch.cache(res) : Promise.resolve(res))
+        .then(res => is.html(req.url) ? Head.add(res) : res)
+    ).catch(er => console.error(req.url) ?? console.error(er));
 }
-fetch.cache = res => caches.open(is.parts(res.url) ? 'parts' : 'V3')
+fetch.cache = res => caches.open(is.part(res.url) ? 'parts' : 'V3')
     .then(cache => cache.put(res.url.replace(/[?#].*$/, ''), res.clone()))
     .then(() => res);
 
@@ -58,8 +79,7 @@ const Head = {
     cache: () => caches.open('V3').then(cache => cache.put(Head.url, new Response(Head.code))),
     fetch: () => caches.match(Head.url).then(resp => resp.text()),
 
-    add: async resp => (resp?.headers.get('content-type') || '').includes('text/html') ? 
-        new Response(await Head.fetch() + await resp.text(), Head.response(resp)) : resp,
+    add: async resp => new Response(await Head.fetch() + await resp.text(), Head.response(resp)),
             
     response: ({status, statusText, headers}) => ({status, statusText, headers})
 }
