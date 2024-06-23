f3b60962e2919d70c07513f21ec4f876ffeadf16
bbx <bbx@bbx.com>
2024-06-23 23:05:46 +0800

    3.0

--- include/common.js
+++ include/common.js
@@ -41,7 +41,7 @@ addEventListener('DOMContentLoaded', () => {
                 drop.to.return();
             }
         });
-        Q('nav').style.setProperty('--safari', '3em');
+        setTimeout(() => Q('nav').style.setProperty('--safari', '3em'));
     }
     Q('[popover]')?.addEventListener('click', ev => ev.target.closest('[popover]').hidePopover());
 });
